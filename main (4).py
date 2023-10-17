74f09fd9837eee186746451d0bb872496e4fecdd
Ragul15022004 <144645989+Ragul15022004@users.noreply.github.com>
2023-10-17 09:27:21 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,36 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+
+
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+
+
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
